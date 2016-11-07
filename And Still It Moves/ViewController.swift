734fc1f2eda345ff59c6dcdf390c6bf7a74c0437
Student <s525796@nw39101.nwmissouri.edu>
2016-11-07 16:55:28 -0600

    added moving gesture .....should implement double tap gesture

--- And Still It Moves/ViewController.swift
+++ And Still It Moves/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myIV: UIImageView!
+    var startPoint:CGPoint!
+    var tapGR:UITapGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tapGR = UITapGestureRecognizer(target: self, action: Selector("tapper:"))
+        tapGR.numberOfTapsRequired = 2 // Only triggered by a double tap
+        myIV.addGestureRecognizer(tapGR) // Associate tapGR with myUIView2
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handlePan(panGR:UIPanGestureRecognizer) -> Void {
+        let translation:CGPoint = panGR.translationInView(myIV)
+        print(translation)
+        if panGR.state == .Began {
+            print("Here we go! -- \(translation)")
+            startPoint = myIV.frame.origin
+        }
+            else if panGR.state == .Ended {
+                print("There we went! -- \(translation)")
+            }
+        myIV.frame.origin.x = startPoint.x + translation.x
+        myIV.frame.origin.y = startPoint.y + translation.y
+        }
+    func tapper(tapGR:UITapGestureRecognizer)->Void{
+        print(tapGR.description)
+        
+    }
 }
 
