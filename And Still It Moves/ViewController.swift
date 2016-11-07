12f0aac1d523f518a15ce5d962418c09d1d33963
Student <s525796@nw39101.nwmissouri.edu>
2016-11-07 16:30:36 -0600

    Initial Commit

---
+++ And Still It Moves/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  And Still It Moves
+//
+//  Created by Bandaru,Sreekanth on 11/7/16.
+//  Copyright © 2016 Bandaru Sreekanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
